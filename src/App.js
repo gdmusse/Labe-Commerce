0e8cd19f3962b69fce1ab97bc8e9fa35961662fa
Giselle Rosa <gisellenrosa@gmail.com>
2021-03-17 18:56:14 -0300

    .

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
 import React from "react";
 import { Produtos } from "./components/Produtos/Produtos";
-import Carrinho from "./components/Carrinho/Carrinho"
+import Carrinho from "./components/Carrinho/Carrinho";
+/* import {Filtros} from "./components/Filtros/Filtros"; */
+import "./App.css";
+import styled from "styled-components";
+
+
+const Commerce = styled.div `
+max-width:100vw;
+max-height:100vh;
+display:grid;
+grid-template-columns:1fr 3fr 1fr;
+
+`
+
+
+
 import Filtros from "./components/Filtros/Filtros";
 import "./App.css";
 import styled from "styled-components";
@@ -11,31 +26,36 @@ const DivPai = styled.div`
 `;
 const listaDeProdutos = [
   {
+    codigoProduto: 1,
     nomeProduto: "Produto 1",
     fotoProduto: "https://picsum.photos/200/150?random=1",
     precoProduto: 50,
   },
   {
+    codigoProduto: 2, 
     nomeProduto: "Produto 2",
     fotoProduto: "https://picsum.photos/200/150?random=2",
     precoProduto: 150,
   },
   {
+    codigoProduto: 3, 
     nomeProduto: "Produto 3",
     fotoProduto: "https://picsum.photos/200/150?random=3",
     precoProduto: 250,
   },
-  {
+  { codigoProduto: 4, 
     nomeProduto: "Produto 4",
     fotoProduto: "https://picsum.photos/200/150?random=4",
     precoProduto: 30,
   },
   {
+    codigoProduto: 5, 
     nomeProduto: "Produto 5",
     fotoProduto: "https://picsum.photos/200/150?random=5",
     precoProduto: 80,
   },
   {
+    codigoProduto: 6, 
     nomeProduto: "Produto 6",
     fotoProduto: "https://picsum.photos/200/150?random=6",
     precoProduto: 500,
@@ -45,10 +65,16 @@ const listaDeProdutos = [
 export default class App extends React.Component {
   state = {
     produtos: listaDeProdutos,
+    carrinho: [],
+
     inputValorMin: "",
     inputValorMax: "",
     inputBuscaNome: "",
   };
+
+  AdicionarProduto = (produto)=>{
+    
+  }
 
   handleInputChangeMin = (event) => {
     this.setState({ inputValorMin: event.target.value });
@@ -64,7 +90,8 @@ export default class App extends React.Component {
 
   render() {
     return (
-      <DivPai>
+      <Commerce>
+        <DivPai>
         <div>
           <Filtros
             inputValorMin={this.state.inputValorMin}
@@ -83,11 +110,14 @@ export default class App extends React.Component {
             inputBuscaNome={this.state.inputBuscaNome}
           />
         </div>
-
-        <div>
-          <Carrinho />
-        </div>
       </DivPai>
+        <div>
+          <Produtos produtos={this.state.produtos} />
+        </div>
+        <div>
+          <Carrinho/>
+        </div>
+      </Commerce>
     );
   }
 }
