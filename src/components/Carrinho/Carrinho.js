eca7e7b81415377c7446c600b4c09b27b6334287
Giselle Rosa <gisellenrosa@gmail.com>
2021-03-18 17:43:27 -0300

    carrinho

--- src/components/Carrinho/Carrinho.js
+++ src/components/Carrinho/Carrinho.js
@@ -1,53 +1,53 @@
 import React from "react";
 import styled from "styled-components";
 
-
-const CardCarrinho = styled.div `
-border: 2px solid black;
-width: 200px;
-height:800px;
-margin:55px 0 ;
-
+const CardCarrinho = styled.div`
+  border: 2px solid black;
+  width: 250px;
+  height: 800px;
+  margin: 55px 0;
 `;
-const TitleDiv = styled.div `
-display: grid;
-grid-template-columns: 1fr 1fr 1fr ;
-
+const TitleDiv = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
 `;
 
 const ValorCompra = styled.div`
-display:flex;
-margin-top:60px;
+  display: flex;
+  margin-top: 60px;
 `;
 
 const TituloCarrinho = styled.h1`
-text-align:center;
+  text-align: center;
 `;
-export default class Carrinho extends React.Component{
-
-render(){
-    return(
-        <CardCarrinho>
-            <TituloCarrinho>Carrinho</TituloCarrinho>
-            <TitleDiv>
-                <div> Quantidade </div> 
-                <div> Produto </div>
-                <div>Excluir</div>
-                <div>{this.props.quantidade} </div> 
-                <div>{this.props.nomeProduto}</div> 
-                <div>{this.props.precoProduto}</div> 
-                <button>X</button>
-               
-            </TitleDiv>
-            <ValorCompra>
-                <div>Valor Total: R$ {this.props.valorTotalCompras} </div>
-                
-            </ValorCompra>
-
-
-
-        </CardCarrinho>
-    )
-}
-
+export default class Carrinho extends React.Component {
+  listaCarrinho = () => {
+    return (
+      this.props.carrinho.length > 0 &&
+      this.props.carrinho.map((produto) => {
+        console.log(produto);
+        return (
+          <TitleDiv>
+            <div> Quantidade </div>
+            <div> Produto </div>
+            <div>Excluir</div>
+            <div>{produto.quantidade}</div> <div>{produto.nomeProduto}</div>{" "}
+            <button>Remover</button>
+          </TitleDiv>
+        );
+      })
+    );
+  };
+  render() {
+    return (
+      <CardCarrinho>
+        <TituloCarrinho>Carrinho</TituloCarrinho>
+        {this.listaCarrinho()}
+        <ValorCompra>
+          <div>Valor Total: </div>
+          <div>00,00</div>
+        </ValorCompra>
+      </CardCarrinho>
+    );
+  }
 }
